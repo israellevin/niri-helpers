15ec073ca16da3e29106e458657f472b9162bdb6
I. <40337+israellevin@users.noreply.github.com>
2026-04-03 00:17:29 +0300

    Polish examples

--- ned/examples/float_magnet.sh
+++ ned/examples/float_magnet.sh
@@ -3,10 +3,11 @@
 MEMORY="/tmp/magnet.memory"
 current_workspace="$(niri msg --json workspaces | jq -r '.[] | select(.is_focused == true) | .id')"
 if [ -f "$MEMORY" ]; then
-    last_focused_workspace="$(tail -n1 "$MEMORY")"
-    if [ "$last_focused_workspace" != "$current_workspace" ]; then
+    previous_workspace="$(tail -n1 "$MEMORY")"
+    rm "$MEMORY" 2>/dev/null
+    if [ "$previous_workspace" ] && [ "$previous_workspace" != "$current_workspace" ]; then
         for window_id in $(niri msg --json windows | jq -r ".[] | \
-            select(.workspace_id == $last_focused_workspace) | \
+            select(.workspace_id == $previous_workspace) | \
             select(.is_floating == true) | \
         .id"); do
             niri msg action move-window-to-workspace --window-id "$window_id" --focus false \
--- ned/examples/magnet.sh
+++ ned/examples/magnet.sh
@@ -1,20 +1,14 @@
 #!/usr/bin/sh -e
 [ "$NED_PID" ] || exec ned Workspace "$0 $*"
 MEMORY="/tmp/magnet.memory"
-
-get() {
-    niri msg --json "$1" | jq -r "$2"
-}
-
-current_workspace="$(get workspaces '.[] | select(.is_focused == true) | .id')"
-
+current_workspace="$(niri msg --json workspaces | jq -r '.[] | select(.is_focused == true) | .id')"
 if [ -f "$MEMORY" ]; then
-    previous_workspace="$(tail -n1 "$MEMORY")"
+    previous_workspace="$(tail -n1 "$MEMORY")" || return 0
     rm "$MEMORY" 2>/dev/null
-    if [ "$previous_workspace" != "$current_workspace" ]; then
-        previous_workspace_idx="$(get workspaces '.[] | select(.id == '"$previous_workspace"') | .idx')"
+    if [ "$previous_workspace" ] && [ "$previous_workspace" != "$current_workspace" ]; then
+        previous_workspace_idx="$(niri msg --json workspaces | \
+            jq '.[] | select(.id == '"$previous_workspace"') | .idx')"
         niriu.sh flock --workspace "$previous_workspace_idx" "$@"
     fi
 fi
-
 echo "$current_workspace" > "$MEMORY"
